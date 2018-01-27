0acf1bbbbeeab0a2787a4c00155ba32178a62d21
Alejandro Thornton <alejandro.thornton@gmail.com>
2018-01-27 16:46:00 -0500

    Deck Class Update
    Deck constructor created.

--- Deck.java
+++ Deck.java
@@ -36,6 +36,27 @@ public class Deck {
                     case 2:
                         cardlist.add(new Card("Fortify Broadcast", 2, "Multiply the value of any tile by 2.", new Effect('*',2),'X'));
                         break;
+                    case 3:
+                        cardlist.add(new Card("Surge", 3, "Multiply the value of adjacent tiles by 2.",new Effect('*',2),'A'));
+                        break;
+                    case 4:
+                        cardlist.add(new Card("Boost Signal", 4, "Increase the value of any tile",new Effect('+',2),'X'));
+                        break;
+                    case 5:
+                        cardlist.add(new Card("Signal Tower", 5, "No effect.",new Effect('/',0),'/'));
+                        break;
+                    case 6:
+                        cardlist.add(new Card("Dampen Signal", 6, "Reduce the value of any tile by 2.", new Effect('-',2),'X'));
+                        break;
+                    case 7:
+                        cardlist.add(new Card("New Equipment",7,"If your hand size is 4 or higher, draw until you have 7 cards.",new Effect('D',0),'S'));
+                        break;
+                    case 8:
+                        cardlist.add(new Card("Multi-Point Broadcast", 8, "Non-adjacent tiles are increased by 2.", new Effect('+',2), 'N'));
+                        break;
+                    case 9:
+                        cardlist.add(new Card("Broad Spectrum Dampener", 9, "Reduce adjacent tiles by 2.", new Effect('-', 2), 'A'));
+                        break;
                 }
             }
         }
